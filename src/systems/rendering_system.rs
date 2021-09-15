3c9a2b8d29cec0b8b1078de1be1c3f41b9f6e688
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-09-15 23:33:00 +0200

    feat: batch rendering added

--- src/systems/rendering_system.rs
+++ src/systems/rendering_system.rs
@@ -1,7 +1,10 @@
 use ggez::graphics::{self, Color, DrawParam, Image};
 use ggez::mint as mi;
-use ggez::Context;
+use ggez::{timer, Context};
+use graphics::spritebatch::SpriteBatch;
+use itertools::Itertools;
 use specs::{join::Join, Read, ReadStorage, System};
+use std::collections::HashMap;
 use std::time::Duration;
 
 use crate::components::{Position, Renderable, RenderableKind};
@@ -29,14 +32,12 @@ impl RenderingSystem<'_> {
         .expect("expect drawing queued text");
     }
 
-    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> Image {
+    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> String {
         let path_index = match renderable.kind() {
             RenderableKind::Static => 0,
             RenderableKind::Animated => ((delta.as_millis() % 1000) / 250) as usize,
         };
-
-        let image_path = renderable.path(path_index);
-        Image::new(self.context, image_path).expect("expected image")
+        renderable.path(path_index)
     }
 }
 
@@ -54,21 +55,50 @@ impl<'a> System<'a> for RenderingSystem<'a> {
 
         // Should change that to FlaggedStorage to maintained a sorted Entity list
         // https://specs.amethyst.rs/docs/tutorials/12_tracked.html
-        let mut rendering_data = (&position, &renderables).join().collect::<Vec<_>>();
-        rendering_data.sort_by_key(|&k| k.0.z);
+        let rendering_data = (&position, &renderables).join().collect::<Vec<_>>();
+        let mut rendering_batches: HashMap<u8, HashMap<String, Vec<DrawParam>>> = HashMap::new();
 
         for (position, renderable) in rendering_data.iter() {
-            let image = self.get_image(renderable, time.delta);
+            let image_path = self.get_image(renderable, time.delta);
+
             let x = position.x as f32 * TILE_WIDTH;
             let y = position.y as f32 * TILE_WIDTH;
+            let z = position.z;
 
             let draw_params = DrawParam::new().dest(mi::Point2 { x, y });
-            graphics::draw(self.context, &image, draw_params).expect("expected render");
+
+            rendering_batches
+                .entry(z)
+                .or_default()
+                .entry(image_path)
+                .or_default()
+                .push(draw_params);
+            // graphics::draw(self.context, &image, draw_params).expect("expected render");
         }
+
+        // Iterate spritebatches ordered by z and actually render each of them
+        for (_z, group) in rendering_batches
+            .iter()
+            .sorted_by(|a, b| Ord::cmp(&a.0, &b.0))
+        {
+            for (image_path, draw_params) in group {
+                let image = Image::new(self.context, image_path).expect("expected image");
+                let mut sprite_batch = SpriteBatch::new(image);
+
+                for draw_param in draw_params.iter() {
+                    sprite_batch.add(*draw_param);
+                }
+
+                graphics::draw(self.context, &sprite_batch, graphics::DrawParam::new())
+                    .expect("expected render");
+            }
+        }
+
         // Render any text
         self.draw_text(&gameplay.state.to_string(), 525.0, 80.0);
         self.draw_text(&gameplay.moves_count.to_string(), 525.0, 100.0);
-
+        let fps = format!("FPS: {:.0}", timer::fps(self.context));
+        self.draw_text(&fps, 525.0, 120.0);
         graphics::present(self.context).expect("expected to present");
     }
 }
